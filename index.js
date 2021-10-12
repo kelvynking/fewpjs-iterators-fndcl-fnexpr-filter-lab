d125eb87af1776ba1fcf717fe659d84378c7dc26
kelvynking <kelvyn.m.king@gmail.com>
2021-10-12 11:32:17 +1100

    Created a function findMatching which uses filter() as specified.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(arr, str) {
+    return arr.filter(match => match.toLowerCase() === str.toLowerCase());
+    }
