6fc5e6d194ae6e0830731fd9ffd1f6c41aecee86
kelvynking <kelvyn.m.king@gmail.com>
2021-10-19 12:07:51 +1100

    Created function fuzzyMatchwhich returns all drivers whose names begin with the provided letters.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
 function findMatching(arr, str) {
     return arr.filter(match => match.toLowerCase() === str.toLowerCase());
     }
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(match => match.toLowerCase().indexOf(str.toLowerCase()) === 0);
+}
